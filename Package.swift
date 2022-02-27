847323f3ff332f94f294970f72f2fac76e16b02b
Darius Miclaus <mdarius13@icloud.com>
2022-02-27 12:40:40 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,33 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
+/// ripemd160-Swift Package
 let package = Package(
+    /// Package Name
     name: "ripemd160-Swift",
+    
+    /// Package Products
+    /// Products define the executables and libraries a package produces, and make them visible to other packages.
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ripemd160-Swift",
-            targets: ["ripemd160-Swift"]),
+            targets: ["ripemd160-Swift"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    
+    /// Package Targets
+    /// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    /// Targets can depend on other targets in this package, and on products in packages this package depends on.
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        /// Main library
         .target(
             name: "ripemd160-Swift",
-            dependencies: []),
+            path: "./Sources/"
+        ),
+        
+        /// Unit tests
         .testTarget(
-            name: "ripemd160-SwiftTests",
-            dependencies: ["ripemd160-Swift"]),
+            name: "Tests",
+            dependencies: ["ripemd160-Swift"],
+            path: "./Tests/")
     ]
 )
